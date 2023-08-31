4a05b03c92c8d947b6fb90cf18f770c6dd42b46e
LAVANYA J C <139207138+Lavanyajc@users.noreply.github.com>
2023-08-31 10:03:23 +0530

    Update project 5 password generator 3 ways.py

--- project 5 password generator.py
+++ project 5 password generator.py
@@ -2,32 +2,34 @@ import random
 letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers=['1','2','3','4','5','6','7','8','9']
 symbols= ['!','@','$''%','^','&','*','(',')','_','+']
-         
+
+
+#input functions         
 n_letters=int(input("Enter the number of letters you need"))
 n_numbers=int(input("enter the count of numbers you need"))
 n_symbols=int(input("enter the number of symbols you need"))
 
 
 #EASY LEVEL PASSWORD generating with fixed password length using inputs
+
 #password=""
-#for char in range(1,n_letters+1): # if we considser 4 letters ,2numbers and 2 symbols in outr password then this code .
+#for char in range(1,n_letters+1):
   #random_char = random.choice(letters)
   #password += random_char
   
-
 #for n in range(1,n_numbers+1):
  # random_n = random.choice(numbers)
  # password += random_n
  
-
 #for s in range(1,n_symbols+1):
  # random_s=random.choice(symbols)
  # password += random_s
-  
 #print(password)
 
-#hard level generating password with random length without inputs so make input function lines as comments
-#print(len(letters))
+
+
+'''#hard level generating password with random length without inputs so make input function lines as comments
+
 #password=''
 #for char in range(1,len(letters)):
  # password += random.choice(letters) 
@@ -37,16 +39,16 @@ n_symbols=int(input("enter the number of symbols you need"))
 
 #for char in range(1,len(symbols)):
  # password += random.choice(symbols) 
+#print(password)'''  you can use this best code for password generating that i mentioned between triple quotes..you can remove hash at everyline in order to run the code and dont use input funtion at beginning.
 
-#print(password)
+
 
 
 
 #Other method to do it using inputs with unorder .....first method and this both are same except here in this method  we took list and it is unordered
+
 password_list=[]
 random.shuffle(password_list)
-
-
 
 for char in range(1,n_letters+1):
   password_list+=(random.choice(letters))
@@ -56,6 +58,5 @@ for char in range(1,n_numbers+1):
 
 for char in range(1,n_symbols+1):
   password_list+=(random.choice(symbols))
-
 
 print(password_list)
